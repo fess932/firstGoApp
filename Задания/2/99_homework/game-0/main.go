7fe5aa2384b31194e44a6ce599107ca722e3107d
fess932 <fess932@gmail.com>
2018-09-05 11:18:00 +0300

    s

--- Задания/2/99_homework/game-0/main.go
+++ Задания/2/99_homework/game-0/main.go
@@ -4,11 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	initGame()
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	fmt.Println("Добро пожаловать в игру!")
+	text, _, _ := reader.ReadLine()
+	arr := strings.Split(string(text[:]), " ")
+	if arr[0] == "осмотреться" {
+		fmt.Println("ты находишься на кухне, на столе чай, надо собрать рюкзак и идти в универ. можно пройти - коридор")
+	}
+}
+func initGame() {
+
+}
+
+func handleCommand(command string) string {
+
+	return command
 }
