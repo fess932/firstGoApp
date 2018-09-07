b8047c5c13df64a0a04a1cdf61a6244b13ae1819
fess932 <fess932@gmail.com>
2018-09-07 13:37:23 +0300

    ыв

--- Задания/2/99_homework/game-0/main.go
+++ Задания/2/99_homework/game-0/main.go
@@ -13,15 +13,17 @@ type player struct {
 }
 
 type room struct {
-	actions map[string]string
+	about   string
+	route   map[string]room
+	actions map[string]func()
 }
 
 func (p *player) watch() {
-	fmt.Println(p.position.actions["ABOUT"])
+	fmt.Println(p.position.about)
 }
 
 func (p *player) move(s string) {
-	p.position = rooms[s]
+	If p.position = rooms[s]
 }
 
 func (p *player) use(o, s string) {
@@ -29,20 +31,27 @@ func (p *player) use(o, s string) {
 }
 
 var rooms = map[string]room{
-	"KITCHEN": {
-		actions: map[string]string{
-			"USE":   "ANOE",
-			"ABOUT": "ты находишься на кухне, на столе чай, надо собрать рюкзак и идти в универ. можно пройти - коридор",
+	"кухня": {
+		about: "ты находишься на кухне, на столе чай, надо собрать рюкзак и идти в универ. можно пройти - коридор",
+		actions: map[string]func(){
+			"USE": func() {},
+		},
+		route: {
+			"коридор": rooms["коридор"],
 		},
 	},
-	"HALL": {
-		actions: map[string]string{
-			"ABOUT": "ничего интересного. можно пройти - кухня, комната, улица",
+	"коридор": {
+		about: "ничего интересного. можно пройти - кухня, комната, улица",
+		actions: map[string]func(){
+			"USE": func() {},
+		},
+		route: {
+			"кухня": rooms["кухня"],
 		},
 	},
 }
 
-var mainPlayer = player{name: "HERO", position: rooms["KITCHEN"]}
+var mainPlayer = player{name: "ты", position: rooms["кухня"]}
 
 // var rooms = []room {
 // 	room{name: "main", answers: ["осмотреться"]}
@@ -61,7 +70,7 @@ func initGame() {
 	// 	fmt.Println("ты находишься на кухне, на столе чай, надо собрать рюкзак и идти в универ. можно пройти - коридор")
 	// }
 	mainPlayer.watch()
-	mainPlayer.move("HALL")
+	mainPlayer.move("коридор")
 	mainPlayer.watch()
 	handleCommand(string(text))
 }
@@ -69,13 +78,20 @@ func initGame() {
 func handleCommand(input string) string {
 	arr := strings.Split(input, " ")
 	command := arr[0]
-	// param1 := arr[1]
-	// param2 := arr[2]
+	var param1, param2 string
+
+	if len(arr) == 2 {
+		param1 = arr[1]
+	}
+	if len(arr) == 3 {
+		param2 = arr[2]
+	}
+
 	switch {
 	case command == "осмотреться":
 		mainPlayer.watch()
-		// case command == "идти":
-		// 	mainPlayer.move(param1)
+	case command == "идти":
+		mainPlayer.move(param1)
 	}
 	return command
 }
